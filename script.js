992a0c6418d1c7d49eab55172c535b8eee65e136
Amin-Sahebi37 <84955792+Amin-Sahebi37@users.noreply.github.com>
2021-10-31 20:45:33 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full-stack Web Developer|", "Full-stack Software Engineer"],
+        strings: ["Full-stack Web Developer|", "Full-stack Software Engineer| ", "Software Tester"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full-stack Web Developer|", "Full-stack Software Engineer"],
+        strings: ["Full-stack Web Developer|", "Full-stack Software Engineer| ", "Software Tester"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
